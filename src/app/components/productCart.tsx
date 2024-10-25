6376be18e884c24ec2190ad10f037ee4652762f1
SaqibAmin369 <saqib.hyd369@gmail.com>
2024-10-25 20:35:22 +0500

    main files

---
+++ src/app/components/productCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddToCart from './addToCart';
+
+const ProductCart = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  )
+}
+export default ProductCart
